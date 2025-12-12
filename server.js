c56f53b002a0ad63a27a3fec956280ca77a35d21
MkmSys <120376858+MkmSys@users.noreply.github.com>
2025-12-12 23:06:07 +0700

    Add files via upload

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const server = http.createServer(app);
+const io = new Server(server, { cors: { origin: '*' } });
+
+io.on('connection', (socket) => {
+    console.log('User connected:', socket.id);
+    
+    socket.on('join-room', ({ roomId, user }) => {
+        socket.join(roomId);
+        socket.to(roomId).emit('user-joined', user);
+    });
+    
+    socket.on('signal', ({ to, data }) => io.to(to).emit('signal', { from: socket.id, data }));
+    
+    socket.on('chat-message', ({ roomId, text }) => {
+        io.to(roomId).emit('chat-message', { text });
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected:', socket.id);
+    });
+});
+
+server.listen(4000, () => console.log('Server running on port 4000'));
