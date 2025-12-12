52d071e4653df4a78222e589a10c60e6d8379ea3
MkmSys <120376858+MkmSys@users.noreply.github.com>
2025-12-13 00:16:16 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-<server.js content omitted for brevity>
+const path = require("path");
+const express = require("express");
+const app = express();
+
+const staticPath = path.join(__dirname, "static");
+app.use(express.static(staticPath));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(staticPath, "index.html"));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server running on", PORT));
